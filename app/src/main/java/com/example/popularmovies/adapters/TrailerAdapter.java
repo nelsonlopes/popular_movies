1f654028ae4a91cff046b9010b7615802ff1fa6a
Nelson Lopes <nelopes@expressglass.pt>
2019-09-23 18:32:38 +0100

    feat: Add layout for trailer item, with play button and textview for the trailer name. Change the RecyclerView adapter to load this new view

--- app/src/main/java/com/example/popularmovies/adapters/TrailerAdapter.java
+++ app/src/main/java/com/example/popularmovies/adapters/TrailerAdapter.java
@@ -3,10 +3,13 @@ package com.example.popularmovies.adapters;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.example.popularmovies_stage1.R;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.popularmovies.model.Trailer;
@@ -22,11 +25,14 @@ public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.MyViewHo
 
     // Provide a reference to the views for each data item
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView textView;
+        //public TextView textView;
+        public View view;
 
-        public MyViewHolder(TextView v) {
+        //public MyViewHolder(TextView v) {
+        public MyViewHolder(View v) {
             super(v);
-            textView = v;
+            //textView = v;
+            view = v;
         }
     }
 
@@ -39,7 +45,8 @@ public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.MyViewHo
     @Override
     public TrailerAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        TextView v = new TextView(mContext);
+        //TextView v = new TextView(mContext);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.trailer_item, parent, false);
 
         TrailerAdapter.MyViewHolder vh = new TrailerAdapter.MyViewHolder(v);
 
@@ -52,7 +59,27 @@ public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.MyViewHo
         // - get element from the dataset at this position
         // - replace the contents of the view with that
 
-        holder.textView.setOnClickListener(new View.OnClickListener() {
+        /*holder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Trailer trailer = getItem(holder.getAdapterPosition());
+
+                Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(YOUTUBE_APP_BASE_URI +
+                        trailer.getKey()));
+                Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(YOUTUBE_WEB_BASE_URL + trailer.getKey()));
+                try {
+                    mContext.startActivity(appIntent);
+                } catch (ActivityNotFoundException ex) {
+                    mContext.startActivity(webIntent);
+                }
+            }
+        });*/
+        TextView trailerNameTv = holder.view.findViewById(R.id.tv_trailer_name);
+        Button playBtn = holder.view.findViewById(R.id.btn_play);
+
+        trailerNameTv.setText(trailers.get(position).getName());
+        playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Trailer trailer = getItem(holder.getAdapterPosition());
@@ -69,7 +96,7 @@ public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.MyViewHo
             }
         });
 
-        holder.textView.setText(trailers.get(position).getName());
+        //holder.textView.setText(trailers.get(position).getName());
     }
 
     // Return the size of the dataset (invoked by the layout manager)
